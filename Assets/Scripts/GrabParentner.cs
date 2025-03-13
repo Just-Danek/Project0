21305a3a08c528d6c03777e09a8e6f3741f40333
Leelik12 <fatyanov2006@gmail.com>
2025-03-13 15:36:40 +0300

    Added VR
    animation hand, and some grab

---
+++ Assets/Scripts/GrabParentner.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrabParentner : MonoBehaviour
+{
+    public void OnGrab(SelectEnterEventArgs args)
+    {
+
+    }
+    public void OnUngrab(SelectExitEventArgs args) 
+    { 
+    
+    }
+}
---
+++ Assets/Scripts/HandAnimation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+
+public class HandAnimation : MonoBehaviour
+{
+    [SerializeField]
+    XRInputValueReader<float> m_TriggerInput;
+
+    [SerializeField]
+    XRInputValueReader<float> m_GripInput;
+
+    [SerializeField] 
+    Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Trigger", m_TriggerInput.ReadValue());
+        animator.SetFloat("Grip", m_GripInput.ReadValue());
+    }
+}
