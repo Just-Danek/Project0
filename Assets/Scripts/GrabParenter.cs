14d0112c09a08fe59513cc6d4d886e55c167c990
Leelik12 <fatyanov2006@gmail.com>
2025-04-01 18:55:13 +0300

    update

--- Assets/Scripts/GrabParenter.cs
+++ Assets/Scripts/GrabParenter.cs
@@ -3,14 +3,18 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class GrabParenter : MonoBehaviour
 {
+    Vector3 gun;
     public void OnGrab(SelectEnterEventArgs args)
     {
+        gun = args.interactableObject.transform.localScale;
         Debug.Log("Взял");
-        args.interactableObject.transform.position = args.interactorObject.transform.position;
+        args.interactableObject.transform.SetParent(args.interactorObject.transform);
+        args.interactableObject.transform.localScale = gun;
     }
     public void OnUngrab(SelectExitEventArgs args)
     {
+        args.interactableObject.transform.localScale = gun;
         Debug.Log("Отпустил");
-        args.interactableObject.transform.position = args.interactableObject.transform.position;
+        args.interactableObject.transform.SetParent(null);
     }
 }
