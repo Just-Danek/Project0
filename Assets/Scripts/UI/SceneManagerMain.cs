cb33c4331fd68e97b25ffbf4610b0a0171eeeaa5
FierBlox <104228053+FierBlox@users.noreply.github.com>
2025-04-19 00:01:39 +0300

    Revert "Merge branch 'main' into AI-2"
    This reverts commit 5d6be4fb5d334bf69171e0d220176e6fedde5ee4, reversing
    changes made to 0cd9cb6cb81701c732508c2394f101d75a143d9e.

--- Assets/Scripts/UI/SceneManagerMain.cs
+++ Assets/Scripts/UI/SceneManagerMain.cs
@@ -3,32 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerMain : MonoBehaviour
 {
-    [SerializeField] public GameObject Menu;
-    [SerializeField] public GameObject Setting;
-    [SerializeField] public GameObject Achievments;
     public void Begin()
     {
         SceneManager.LoadSceneAsync("TheFirstLevel");
         Debug.Log("Загрузка первого уровня");
-    }
-    public void Settings()
-    {
-        Menu.SetActive(false);
-        Setting.SetActive(true);
-    }
-    public void Achiev()
-    {
-        Menu.SetActive(false);
-        Achievments.SetActive(true);
-    }
-    public void Quit()
-    {
-        Application.Quit();
-    }
-    public void Back2Menu()
-    {
-        Achievments.SetActive(false);
-        Setting.SetActive(false);
-        Menu.SetActive(true);
     }
 }
