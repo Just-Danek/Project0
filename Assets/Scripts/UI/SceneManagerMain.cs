7e76126b440ef0c6f82cbcd02d252b632e538bfa
Leelik12 <fatyanov2006@gmail.com>
2025-04-21 14:36:03 +0300

    nastroenMain

--- Assets/Scripts/UI/SceneManagerMain.cs
+++ Assets/Scripts/UI/SceneManagerMain.cs
@@ -3,9 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerMain : MonoBehaviour
 {
+    [SerializeField] public GameObject Main;
+    [SerializeField] public GameObject Setting;
+    [SerializeField] public GameObject Achievm;
     public void Begin()
     {
         SceneManager.LoadSceneAsync("TheFirstLevel");
         Debug.Log("Загрузка первого уровня");
+    }
+    public void Setti()
+    {
+        Main.SetActive(false);
+        Setting.SetActive(true);
+        Debug.Log("Переход в настройки");
+    }
+    public void Achie()
+    {
+        Main.SetActive(false);
+        Achievm.SetActive(true);
+        Debug.Log("Переход в достижения");
+    }
+    public void Bach2Main()
+    {
+        Main.SetActive(true);
+        Achievm.SetActive(false);
+        Setting.SetActive(false);
+        Debug.Log("Переход в достижения");
+    }
+    public void Exi()
+    {
+        Debug.Log("Выход");
+        Application.Quit();
     }
 }
