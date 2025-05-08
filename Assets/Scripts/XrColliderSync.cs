c2c0c4f0e9ce4fc6f6553c21436406a5440359ce
Leelik12 <fatyanov2006@gmail.com>
2025-05-08 23:25:06 +0300

    WalkingFix

--- Assets/Scripts/XrColliderSync.cs
+++ Assets/Scripts/XrColliderSync.cs
@@ -5,6 +5,8 @@ public class XRBodyColliderSync : MonoBehaviour
     [Header("Collider")]
     [SerializeField] private CharacterController capsuleCollider;
     [SerializeField] public GameObject Locomotion;
+    [SerializeField] public GameObject PosCamers;
+    [SerializeField] public GameObject PosBelt;
     private void Reset()
     {
         capsuleCollider = GetComponent<CharacterController>();
@@ -12,6 +14,10 @@ public class XRBodyColliderSync : MonoBehaviour
 
     private void LateUpdate()
     {
-        capsuleCollider.center = new Vector3(Locomotion.transform.localPosition.x, capsuleCollider.center.y, Locomotion.transform.localPosition.z);
+        //Locomotion.transform.rotation = PosCamers.transform.rotation;
+        //capsuleCollider.transform.rotation = Locomotion.transform.rotation;
+        Locomotion.transform.localPosition = new Vector3(-PosCamers.transform.localPosition.x, capsuleCollider.center.y, PosCamers.transform.localPosition.z);
+        capsuleCollider.center = new Vector3(-Locomotion.transform.localPosition.x, capsuleCollider.center.y, Locomotion.transform.localPosition.z);
+        PosBelt.transform.localPosition = new Vector3(-Locomotion.transform.localPosition.x, PosBelt.transform.localPosition.y, Locomotion.transform.localPosition.z);
     }
 }
